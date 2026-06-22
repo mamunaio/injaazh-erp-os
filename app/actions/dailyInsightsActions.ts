ff55186542c4285ee7c7318762f7fa304c9756e0
mamunaio <mamunaio@users.noreply.github.com>
2026-06-22 12:19:07 +0600

    Fix connectToDatabase import

--- app/actions/dailyInsightsActions.ts
+++ app/actions/dailyInsightsActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { connectToDatabase } from '@/lib/mongodb';
+import connectToDatabase from '@/lib/mongodb';
 import { IslamicInsight } from '@/models/IslamicInsight';
 import { revalidatePath } from 'next/cache';
 
