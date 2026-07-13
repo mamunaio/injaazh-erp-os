4d2b0dfa7fd65ec458f5e9ace3f0160330d3b743
Mamun <mamunahmed24.ma@gmail.com>
2026-07-13 14:54:42 +0600

    chore: remove fix.js script

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('components/leads/OutreachComposerModal.tsx', 'utf8');
-c = c.replace(/\\`/g, '`');
-c = c.replace(/\\\$/g, '$');
-fs.writeFileSync('components/leads/OutreachComposerModal.tsx', c);
